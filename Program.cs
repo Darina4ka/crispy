6fff56369ea0491318790175ad0edf6f36919365
Daria <dashamelnik293@gmail.com>
2022-09-09 18:28:30 +0300

    done third task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine ("Введите целое число");
+try
+{
+    int N = Convert.ToInt32 (Console.ReadLine ());
+    int i = 1;
+    while (i<=N)
+    {                                                               
+        Console.WriteLine (i*i*i);
+        i++;
+    }
+}
+catch 
+{
+     Console.WriteLine ("Нужно ввести целое число!");
+}
