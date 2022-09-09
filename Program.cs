82145b8f59d623b79afb58854cde196512d23781
Daria <dashamelnik293@gmail.com>
2022-09-09 18:10:00 +0300

    done first task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿Console.WriteLine ("Введите пятизначное число");
+try
+{
+    int a = Convert.ToInt32 (Console.ReadLine ());
+    if (a > 9999)
+    {
+        if (a % 10 == a / 10000 && (a % 100) / 10 == (a / 1000) % 10)
+        {
+            Console.WriteLine ("Да");
+        }
+        else 
+        {
+            Console.WriteLine ("Нет");
+        }
+    }
+    else 
+    {
+        Console.WriteLine ("Нужно введите пятизначное число!");
+    }
+}
+catch
+{
+    Console.WriteLine ("Нужно введите пятизначное число!");
+};
